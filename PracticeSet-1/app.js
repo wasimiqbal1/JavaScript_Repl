122505a3b7975490c44068d90d6ced06d361ffa4
unknown <wasim_iqbal@msn.com>
2022-11-06 01:34:02 +0500

    Practice

---
+++ PracticeSet-1/app.js
@@ -0,0 +1,31 @@
+//Create a variable of type String & add number
+let a = "Wasim"
+let b = 6
+
+console.log( a + b);
+
+
+//Use type of Operator to find the data type
+console.log(typeof(a+b));
+
+//Create a const object in JS Can you change it
+const a1 = {
+    name: "Wasim",
+    section: "B",
+    isPrincipal : false
+}
+
+//Add a new key 
+a1['friend'] = "Shoaib"
+a1['name'] = "Ali"
+
+console.log(a1) 
+
+//Word meaning history
+const dict = {
+    appreciate: "recognize the full worth of",
+    ataraxia:"a state of serene calmness.", 
+    yakka:"work, especially of a strenuous physical kind."
+}
+
+console.log(dict['yakka'])
--- index.js
+++
@@ -1,8 +0,0 @@
-console.log("This function add numbers");
-
-function addThreeNumbers(a, b, c) {
-  return a + b + c;
-}
-
-let c = addThreeNumbers(1, 2, 497)
-console.log(c)
