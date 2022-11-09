f65f136fa834521ce54d6efaacd85b1a5238c607
unknown <wasim_iqbal@msn.com>
2022-11-10 00:40:25 +0500

    Logical Operators

--- Logical Operator/app.js
+++ Logical Operator/app.js
@@ -1,8 +1,19 @@
 let age = prompt("What is your age");
-if (age>10 && age<20){
-    console.log("Your age lies between 10 and 20")
-}
-else{
-    console.log("Your age dosn't lies between 10 and 20")
-}
+    age = Number.parseInt(age)
 
+switch(age){
+    case 12:
+        console.log("Your age is 12")
+        break
+    case 13:
+        console.log("Your age is 13")
+        break
+    case 14:
+        console.log("Your age is 14")
+        break
+    case 15:
+        console.log("Your age is 15")
+        break
+    default:
+        console.log("Your age is not special")    
+}
