9d6a949930cfa2f307faa2a7a4c0d3089ef095eb
unknown <wasim_iqbal@msn.com>
2022-11-10 00:27:52 +0500

    Logical Operators

---
+++ Logical Operator/app.js
@@ -0,0 +1,8 @@
+let age = prompt("What is your age");
+if (age>10 && age<20){
+    console.log("Your age lies between 10 and 20")
+}
+else{
+    console.log("Your age dosn't lies between 10 and 20")
+}
+
