7e2132cc1403043d2007777cd94def52396e8142
unknown <wasim_iqbal@msn.com>
2022-11-12 11:29:25 +0500

    Logical Operator

--- Logical Operator/app.js
+++ Logical Operator/app.js
@@ -1,36 +1,36 @@
-// let age = prompt("What is your age");
-//     age = Number.parseInt(age)
+let age1 = prompt("What is your age");
+    age1 = Number.parseInt(age1)
 
-// switch(age){
-//     case 12:
-//         console.log("Your age is 12")
-//         break
-//     case 13:
-//         console.log("Your age is 13")
-//         break
-//     case 14:
-//         console.log("Your age is 14")
-//         break
-//     case 15:
-//         console.log("Your age is 15")
-//         break
-//     default:
-//         console.log("Your age is not special")    
-// }
+switch(age1){
+    case 12:
+        console.log("Your age is 12")
+        break
+    case 13:
+        console.log("Your age is 13")
+        break
+    case 14:
+        console.log("Your age is 14")
+        break
+    case 15:
+        console.log("Your age is 15")
+        break
+    default:
+        console.log("Your age is not special")    
+}
 
-// // Number is divisible by 2 & 3
+// Number is divisible by 2 & 3
 
-// let num = prompt("What is your age");
-//     nun = Number.parseInt(num)
+let num = prompt("What is your age");
+    nun = Number.parseInt(num)
 
-// if (num % 2 == 0 && num % 3 ==0 ){
-//     console.log("Your number is divisible by 2 & 3 ")
-// }
-// else{
-//     console.log("Your number is not divisible by 2 & 3 ")
-// }
+if (num % 2 == 0 && num % 3 ==0 ){
+    console.log("Your number is divisible by 2 & 3 ")
+}
+else{
+    console.log("Your number is not divisible by 2 & 3 ")
+}
 
 // Use ternary Operator
-let age = 19;
-let a = age > 18 ? "You Can drive" : "You Cannot drive";
+let age = prompt("What is your age");
+let a = age >= 18 ? "You Can drive" : "You Can't drive";
 console.log(a)
