2723a322dd5c8e8acf41b1fc07f64f43bd4e411e
unknown <wasim_iqbal@msn.com>
2022-12-26 23:47:59 +0500

    For Loops

--- Loops/app.js
+++ Loops/app.js
@@ -1,14 +1,26 @@
-// program to generate a multiplication table
+// // program to generate a multiplication table
 
-// take input from the user
-const number = parseInt(prompt('Enter an integer: '));
+// // take input from the user
+// const number = parseInt(prompt('Enter an integer: '));
 
-//creating a multiplication table
-for(let i = 1; i <= 10; i++) {
+// //creating a multiplication table
+// for(let i = 1; i <= 10; i++) {
 
-    // multiply i with number
-    const result = i * number;
+//     // multiply i with number
+//     const result = i * number;
 
-    // display the result
-    console.log(`${number} * ${i} = ${result}`);
+//     // display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+
+let cleanestCities = ["Karachi", "Lahore", "Islamabad", "Peshawar"];
+
+for (let i = 0; i <=  3; i++) {
+  
+  if ("Islamabad" === cleanestCities[i]) {
+      console.log("It's one of the cleanest cities");
+    break;
+
+  }
 }
